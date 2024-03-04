38ef4ee4e320e6d5d4b0f650b7ae071c44e392d8
ipnk0138 <niwa_kazuhiro@interprism.co.jp>
2024-03-04 13:35:41 +0900

    tailwindの適用と入力フォーム,変換ボタンの設置

--- command_text_to_image/src/app/page.tsx
+++ command_text_to_image/src/app/page.tsx
@@ -1,17 +1,54 @@
 'use client'
- 
+
 import { useRouter } from 'next/navigation'
- 
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type FormData = {
+command: string
+}
+/** Submit ボタンを押したときの処理 */
+const onSubmit: SubmitHandler<FormData> = (data) => {
+  // data には FormData 型の値が入っている
+    alert(JSON.stringify(data, null, 2))
+}
 export default function Page() {
-const router = useRouter()
-  return (
-    <>
-        <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-4 rounded-full" onClick={() => router.push('/day')}>
+
+    const router = useRouter()
+    const { handleSubmit, register } = useForm<FormData>()
+
+return (
+    <>{/*
+        <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-4 rounded-full"
+        onClick={() => router.push('/day')}>
         dayへ
         </button>
-        <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-4 rounded-full" onClick={() => router.push('/about')}>
+        <button
+        className="text-3xl font-bold underline"
+        onClick={() => router.push('/about')}>
         aboutへ
-        </button>   
+        </button>
+*/}
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="outline">
+            <input
+            className="underline "
+                {...register('command')}
+                placeholder="Comamnd"
+            />
+            <button
+                className="text-3xl font-semibold underline"
+                type="submit">
+                convert
+            </button>
+            </div>
+
+            <div>
+            ImageArray
+            </div>
+        </form>
     </>
-  )
+    )
+
 }
