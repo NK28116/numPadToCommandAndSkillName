3aefdfea3a77a5504b50ba51b9b943cffbd0cd1c
ipnk0138 <niwa_kazuhiro@interprism.co.jp>
2024-03-06 16:31:24 +0900

    テスト用画像(a.png)の追加とString->string[]->image変換メソッドの登録

--- command_text_to_image/src/app/page.tsx
+++ command_text_to_image/src/app/page.tsx
@@ -8,18 +8,39 @@ type FormData = {
 }
 
 /**
-* 引数inputText:stringをstring[]に
+* 引数inputText:stringをstring[]にして
+* 各要素に対応するPNGを出力
 * @param input
 */
-const convertInputString = (inputText: string) => {
-    return inputText.split("--").map((item) => ({ value: item }));
-}
+const spiltInputArray = (inputText: string) => {
+    return inputText.split("--").map((item) => {
+        if (item.trim() !== "") {
+            return {
+                value: item.trim(),
+                image: `/${item.trim()}.png`
+            };
+        }
+        return null; // エラー処理など
+    });
+};
+
+const convertTextToImage = (inputText: string) => {
+    const inputArray = spiltInputArray(inputText);
+    return inputArray.map((item, index) => {
+        if (item !== null) {
+            return {
+                value: item.value,
+                image: item.image
+            };
+        }
+        return null; // エラー処理など
+    });
+};
 
 export default function Page() {
     const router = useRouter()
     const { handleSubmit, register } = useForm<FormData>()
 
-    let convertImageArray: ({image: string; value: any} | null)[] = []; // 明示的な型指定
 
     /**
     * 文字変換ロジック
@@ -27,23 +48,10 @@ export default function Page() {
     * TODO:日本語も
     * @param data
     */
-    const onSubmit: SubmitHandler<FormData> = (data) => {
-        const inputArray = convertInputString(data.command); // フォームに入力された文字列を処理
-        convertImageArray = inputArray.map((item, index) => { // 既存のconvertImageArrayを更新する
-            if ('value' in item) {
-                return {
-                    value: item.value,
-                    image: `/${item.value}.png`
-                };
-            }
-            return null; // エラー処理など
-        });
-        console.log(convertImageArray); // フォームに入力された文字列に対応する画像の配列を表示
-    };
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form >
                 <div className="outline">
                     <input
                         {...register('command')}
@@ -51,16 +59,19 @@ export default function Page() {
                     />
                     <button
                         className="text-3xl font-semibold underline"
-                        type="submit">
+                        type="submit"
+
+                        >
+
                         Convert
                     </button>
                 </div>
             </form>
-            <div>
-                {/* 画像を出力 */}
-                {convertImageArray.map((item, index) => (
-                    <img key={index} src={item?.image} alt={item?.value} />
-                ))}
+            <div className="debug">
+                 <img src={`/a.png`}  />
+                 <div>
+                 {/*input={`${convertImageArray[0]}`}*/}
+                 </div>
             </div>
         </>
     )
