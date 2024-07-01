001d9430ba412d961b1383da0ddb45b7d10cd537
IPNiwaKazuhiro <k-niwa@cnt.biglobe.co.jp>
2024-07-01 15:11:41 +0900

    区切り文字で繋がった複数の入力に対応

--- command_text_to_image/src/app/api/country/[slug]/route.ts
+++ command_text_to_image/src/app/api/country/[slug]/route.ts
@@ -5,11 +5,16 @@ import { Params } from "next/dist/shared/lib/router/utils/route-matcher"
 
 export async function GET(request: Request, { params }: Params) {
   const { slug } = params
+  const continents = slug.split("--")
 
   try {
-    const countries: country[] = await prisma.country.findMany({ where: { Continent: slug } })
+    const countries: country[] = []
+    for (const continent of continents) {
+      const continentCountries = await prisma.country.findMany({ where: { Continent: continent } })
+      countries.push(...continentCountries)
+    }
     return NextResponse.json(countries)
   } catch (error) {
-    return NextResponse.json(error)
+    return NextResponse.json({ error: error.message }) //error=unknown type
   }
 }
--- command_text_to_image/src/app/api/country/route.ts
+++ command_text_to_image/src/app/api/country/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server"
 import { prisma } from "@/src/lib/prismaClient"
 import { country } from "@prisma/client"
 export async function GET() {
-  // todoテーブルから全件取得
   try {
     const countries: country[] = await prisma.country.findMany()
     return NextResponse.json(countries)
