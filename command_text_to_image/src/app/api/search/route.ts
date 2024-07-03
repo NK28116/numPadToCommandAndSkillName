ec3039bfcec49fb07527a06fcd3a6b0e75752215
IPNiwaKazuhiro <k-niwa@cnt.biglobe.co.jp>
2024-07-03 13:44:18 +0900

    modelの選択を外部に移行

--- command_text_to_image/src/app/api/search/route.ts
+++ command_text_to_image/src/app/api/search/route.ts
@@ -1,14 +1,29 @@
+/**
+ * 期待される結果：/api/search?name=Tokyo&model=city
+ */
+
 import { NextApiRequest, NextApiResponse } from "next"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
+//用いるmodel
+const modelMap = {
+  city: prisma.city,
+  country: prisma.country,
+  // 他のモデルもここに追加できます
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    const { name } = req.query
+    const { name, model } = req.query
 
+    if (!modelMap[model as string]) {
+      return res.status(400).json({ error: "Invalid model name" })
+    }
+    //where の中身でfindUniqueする
     try {
-      const searchData = await prisma.city.findUnique({
+      const searchData = await modelMap[model as string].findUnique({
         where: {
           Name: String(name),
         },
